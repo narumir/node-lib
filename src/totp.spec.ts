25c1925ece2a655bcce76038b85804358712a350
narumir <narumir@hate-real.io>
2022-07-20 18:01:43 +0900

    add jest and write test code

---
+++ src/totp.spec.ts
@@ -0,0 +1,11 @@
+import {
+    TOTP,
+} from "./index";
+
+test("HMAC-SHA-1", () => {
+    const timestamp = 1658305632083;
+    const otp = new TOTP({ algorithm: "sha1" });
+    const token = otp.generate("TL5BNHAVM3OOD4I2", timestamp);
+    expect(token)
+        .toEqual("704305");
+});
